7a2c2227bd9a9bec0f68b7e2cb9c905f96864452
geovanex24 <geovanex24@hotmail.com>
2024-08-29 16:24:57 -0600

    feat: JournalPage, NavBar, and SideBar

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,5 +1,22 @@
 import { Typography } from "@mui/material";
+import { JournalLayout } from "../layout/JournalLayout";
 
 export const JournalPage = () => {
-  return <Typography>JournalPage</Typography>;
+  return (
+    <JournalLayout>
+      <Typography>
+        Aliqua culpa ad non laboris in minim exercitation cillum labore. Dolor
+        incididunt anim anim laboris consectetur quis nulla mollit in sint est.
+        Magna aliqua ea exercitation cillum dolor commodo culpa ut in.
+        Adipisicing id eiusmod magna labore. Consectetur magna ut nisi eiusmod
+        ad nulla nulla commodo laboris commodo qui incididunt ut velit. Elit
+        deserunt veniam est do ullamco consectetur duis sit excepteur minim
+        occaecat. Duis incididunt velit eiusmod incididunt laboris sit laborum
+        tempor ad voluptate incididunt.
+      </Typography>
+
+      {/* NothingSelected */}
+      {/* NoteView */}
+    </JournalLayout>
+  );
 };
