75b697c1de1508cf4164e15e57eda8d13f24d992
geovanex24 <geovanex24@hotmail.com>
2024-08-30 15:00:47 -0600

    feat: add NoteView

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,3 +1,5 @@
+import { IconButton } from "@mui/material";
+import { AddOutlined } from "@mui/icons-material";
 import { JournalLayout } from "../layout/JournalLayout";
 import { NothingSelectedView } from "../views";
 
@@ -16,7 +18,21 @@ export const JournalPage = () => {
       </Typography> */}
 
       <NothingSelectedView />
-      {/* NoteView */}
+      {/* <NoteView /> */}
+
+      <IconButton
+        size="large"
+        sx={{
+          color: "white",
+          backgroundColor: "error.main",
+          ":hover": { backgroundColor: "error.main", opacity: 0.9 },
+          position: "fixed",
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
     </JournalLayout>
   );
 };
